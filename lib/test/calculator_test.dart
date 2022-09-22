eebf45122726e5afc6979d024c2204c8a8f9f8ba
naily26 <nailymasruri@gmail.com>
2022-09-23 05:39:33 +0700

    unit test 1

---
+++ lib/test/calculator_test.dart
@@ -0,0 +1,14 @@
+import 'package:test/test.dart';
+import 'package:unit_testing_1/math/calculator.dart';
+
+void main() {
+  late Calculator _calculator;
+  setUp(() {
+    _calculator = Calculator();
+  });
+
+  test(
+    'calculator.sumTwoNumbers() sum both numbers',
+    () => expect(_calculator.sumTwoNumbers(1, 2), 3)
+  );
+}
